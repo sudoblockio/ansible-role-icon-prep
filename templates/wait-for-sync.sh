b68820f0896a3f1cb6aaca0f58342e44cd9b93d9
rob <robcannonxyz@gmail.com>
2020-09-24 13:51:29 -0700

    adding sync script logic

---
+++ templates/wait-for-sync.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+function wait_for_sync() {
+  until [[ $(curl -s -o /dev/null -w "%{http_code}" localhost:9000/api/v1/status/peer) == "200" ]]; do
+    printf 'Syncing DB\n'
+      sleep 5
+  done
+  echo 'service is available and about to sync'
+}
+
+function sync() {
+  #EvaluateNetwork
+  declare -A array
+  for stateCode in Watch Vote
+  do
+    array[$stateCode]=1
+  done
+
+  until [[ ${array[$(curl -s localhost:9000/api/v1/status/peer | jq -r .state)]} ]]; do
+    printf 'Syncing Blocks\n'
+    block_height=$(curl -s localhost:9000/api/v1/status/peer | jq -r .block_height)
+    echo Blockheight = $block_height
+    printf '\n'
+    sleep 5
+  done
+}
+
+function associate_on_own() {
+  aws ec2 disassociate-address --public-ip  {{ main_ip }} || true
+  aws ec2 associate-address --instance-id {{ this_instance_id }} --public-ip {{ main_ip }}
+}
+
+function re_sync() {
+  until [[ "$(curl -s http://whatismyip.akamai.com/)" == "{{ main_ip }}" ]]; do
+    systemctl restart prep
+    until [[ ${array[$(curl -s localhost:9000/api/v1/status/peer | jq -r .state)]} ]]; do
+      sleep 5
+    done
+  done
+}
+
+REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r`
+export AWS_DEFAULT_REGION=$REGION
+
+wait_for_sync && \
+sync {% if switch_ip_internally %}&& \
+associate_on_own{% endif %} && \
+re_sync
